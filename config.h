6759ac269abefcbdd85dfa5a18a66536fe02922b
Benjamin Brassart <bbrassar@student.42.fr>
2024-07-21 19:49:38 +0200

    Add comments to specify command line switches for `struct config` members

--- config.h
+++ config.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   config.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: benjamin <benjamin@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bbrassar <bbrassar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/30 12:18:51 by benjamin          #+#    #+#             */
-/*   Updated: 2024/06/30 14:17:53 by benjamin         ###   ########.fr       */
+/*   Updated: 2024/07/21 19:46:48 by bbrassar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,19 @@ enum unicode_display {
 };
 
 struct config {
+	/** -a, --debug-syms */
 	unsigned debug_symbols   : 1;
+	/** -g, --extern-only */
 	unsigned extern_only     : 1;
+	/** -p, --no-sort */
 	unsigned no_sort         : 1;
+	/** -r, --reverse-sort */
 	unsigned reverse_sort    : 1;
+	/** -u, --undefined-only */
 	unsigned undefined_only  : 1;
+	/** -V, --version */
 	unsigned display_version : 1;
+	/** --unicode */
 	enum unicode_display unicode_display;
 };
 
