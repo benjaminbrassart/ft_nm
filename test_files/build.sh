6ab166be14a71fa2a937ce7d1374cd98b853b068
Benjamin Brassart <bbrassar@student.42.fr>
2024-07-24 10:16:15 +0200

    Add more target test files

--- test_files/build.sh
+++ test_files/build.sh
@@ -9,7 +9,11 @@ c++ -c -g3 mangled.cpp -o mangled-64le.cpp.o
 
 cc -c -g3 gnu-constructor.c -o gnu-constructor.c.o
 
-rust_targets="i686-unknown-linux-gnu x86_64-unknown-linux-gnu powerpc64-unknown-linux-gnu"
+# i686      => 32-bit little-endian
+# x86_64    => 64-bit little-endian
+# powerpc   => 32-bit big-endian
+# powerpc64 => 64-bit big-endian
+rust_targets="i686-unknown-linux-gnu x86_64-unknown-linux-gnu powerpc-unknown-linux-gnu powerpc64-unknown-linux-gnu x86_64-unknown-freebsd i686-unknown-freebsd"
 
 for target in ${rust_targets}; do
     rustc --emit=obj -C debuginfo=full -C strip=none --target="${target}" hello.rs -o "hello-${target}.rs.o"
