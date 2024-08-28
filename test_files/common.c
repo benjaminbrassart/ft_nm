bb7d764aa3ad037f904ac73ee68cd86f4cce7e7d
Benjamin Brassart <bbrassar@student.42.fr>
2024-08-28 21:37:37 +0200

    Add more symbols to common.c

--- test_files/common.c
+++ test_files/common.c
@@ -1,2 +1,18 @@
-// compile with `-fcommon` to have `b` as common symbol
+// compile with `-fcommon` to have common symbols
 int b;
+int B;
+int _b;
+int _B;
+int __b;
+int __B;
+int ___b;
+int ___B;
+
+int a;
+int A;
+int _a;
+int _A;
+int __a;
+int __A;
+int ___a;
+int ___A;
